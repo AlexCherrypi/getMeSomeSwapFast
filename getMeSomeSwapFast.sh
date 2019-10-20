762dd8182c4a9fe0065a37a9616b5a8c452f4995
KnallbertLp <KnallbertLp@users.noreply.github.com>
2019-10-20 17:35:35 +0200

    Update getMeSomeSwapFast.sh

--- getMeSomeSwapFast.sh
+++ getMeSomeSwapFast.sh
@@ -3,6 +3,11 @@
 # https://github.com/KnallbertLp/getMeSomeSwapFast
 #
 
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit
+fi
+
 sudo apt update -y &&  \
 sudo apt upgrade -y  && \
 sudo apt autoremove -y && \
@@ -11,4 +16,5 @@ sudo dd if=/dev/zero of=/swapfile bs=1024 count=5242880 && \
 sudo chmod 600 /swapfile && \
 sudo mkswap /swapfile && \
 sudo swapon /swapfile && \
-sudo su -c " echo '                /swapfile  swap swap    defaults        0 0' >> /etc/fstab" && sudo  mount -a && sudo reboot 
+sudo su -c " echo '                /swapfile  swap swap    defaults        0 0' >> /etc/fstab" && sudo  mount -a && \
+sudo shutdown -r 1 ; sudo rem gmssf.sh
